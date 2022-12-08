162cb0f070f41e88c3d5c330dd846f7806854a03
Anunaya Kaushik <akaushik@anviam.com>
2022-12-08 18:53:26 +0530

    calendar changes

--- src/app/pages/admin/dashboard/dashboard.component.ts
+++ src/app/pages/admin/dashboard/dashboard.component.ts
@@ -1,7 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { User } from 'src/app/interface/interface';
 import { CommonService } from 'src/app/services/common.service';
 import { fadeInAnimation } from 'src/app/shared/animations/route-animation';
+import {
+  FullCalendarComponent,
+  CalendarOptions,
+  Calendar,
+} from '@fullcalendar/angular';
+import { CommonApiService } from 'src/app/services/common-api.service';
+import tippy from 'tippy.js';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -11,9 +19,18 @@ import { fadeInAnimation } from 'src/app/shared/animations/route-animation';
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  eventDetails!: any[];
   user!: User;
   user_name!: string;
-  constructor(private common: CommonService) {}
+
+  @ViewChild('calendar')
+  calendarComponent!: FullCalendarComponent;
+  calendarOptions: CalendarOptions = {};
+
+  constructor(
+    private common: CommonService,
+    private commonSer: CommonApiService
+  ) {}
 
   ngOnInit(): void {
     this.common.userDetails().subscribe((user: User) => {
@@ -21,5 +38,45 @@ export class DashboardComponent implements OnInit {
       this.user_name = user.first_name + ' ' + user.last_name;
       console.log(user);
     });
+    this.eventData();
   }
+
+  someMethod() {
+    let calendarApi = this.calendarComponent.getApi();
+    calendarApi.next();
+  }
+
+  eventData() {
+    this.commonSer.getEventDetails().subscribe((res: any) => {
+      console.log(res);
+
+      this.eventDetails = res;
+
+      let event: any = [];
+      this.eventDetails.forEach((res) => {
+        event.push({
+          title: res.name + ' ' + res.type,
+          date: res.birthday_date,
+        });
+      });
+      this.calendarOptions = {
+        initialView: 'dayGridMonth',
+        dateClick: this.handleDateClick.bind(this), // bind is important!
+        events: event,
+        eventDidMount: (info) => {
+          tippy(info.el, {
+            content: info.event.title,
+          });
+        },
+      };
+    });
+  }
+
+  
+  handleDateClick(arg: { dateStr: string }) {
+    alert('You Clicked Date ' + arg.dateStr);
+  }
+ 
+  
+  
 }
